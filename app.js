4a75b930b2378fc15a8f982126a13119fa9c09e1
Aldrin Samson <32069182+AldrinSamson@users.noreply.github.com>
2020-10-24 00:16:19 +0800

    Initialize

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express'),
+  path = require('path'),
+  mongoose = require('mongoose'),
+  cors = require('cors'),
+  bodyParser = require('body-parser'),
+  dataBaseConfig = require('./database/db'),
+  app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dataBaseConfig.db, {
+  useNewUrlParser: true
+}).then(() => {
+    console.log('Database connected sucessfully ')
+  },
+  error => {
+    console.log('Could not connected to database : ' + error)
+  }
+)
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+   console.log('Connected to port ' + port)
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.sendStatus(err.status);
+});
---
+++ database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: 'mongodb+srv://admin:IMSRF2020@devcluster0.9ibet.mongodb.net/IMSRF-DEV?retryWrites=true&w=majority'
+  };
